73daa8efe1306643ab66533b6a4a442c46a98ffe
MadRex2000 <spplkk113@gmail.com>
2020-03-22 22:12:23 +0800

    solved Issue #13

--- ntou/kobe/facebookPoster.py
+++ ntou/kobe/facebookPoster.py
@@ -6,14 +6,14 @@ class FbPoster():
     def __init__(self):
         pass
 
-    def poster(id, post):
+    def poster(self, id, post):
         return(graph.put_object(
           parent_object='100177881620069',
           connection_name="feed",
           message='#靠北海大二點零{} \n發文網址：http://www.rexwu.taipei \n{}'.format(id, post),
         ))
 
-    def imgPoster(id, post, img):
+    def imgPoster(self, id, post, img):
         return(graph.put_object(
           parent_object='100177881620069',
           connection_name="feed",
