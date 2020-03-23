450a290483f02decba09929ecbd8474a7c9f89c5
MadRex2000 <spplkk113@gmail.com>
2020-03-23 22:13:03 +0800

    fixed facebook poster

--- ntou/kobe/facebookPoster.py
+++ ntou/kobe/facebookPoster.py
@@ -1,21 +1,15 @@
 import facebook
-graph = facebook.GraphAPI(access_token='token', version="3.0")
+graph = facebook.GraphAPI(access_token='EAAK9493PHmgBAJepxEyL9b04WNPGWOdQ2lhM1Abug2YqZBbz8Om908bxiQzRjcZA3ZBhbbZBb8iM9UbW5ZAJDm49Lar6gZCUK44hFc69RXxN3A94v4YyR6BwTCgPkVzzJqxlszcAAYB9CHT3P5smWPjXcy3qypZAmus16vWqZAhPcQZDZD', version="3.0")
 
 
 class FbPoster():
-    def __init__(self):
-        pass
+    def __init__(self, id, post):
+        self.id = id
+        self.post = post
 
-    def poster(self, id, post):
+    def poster(self):
         return(graph.put_object(
           parent_object='100177881620069',
           connection_name="feed",
-          message='#靠北海大二點零{} \n發文網址：http://www.rexwu.taipei \n{}'.format(id, post),
-        ))
-
-    def imgPoster(self, id, post, img):
-        return(graph.put_object(
-          parent_object='100177881620069',
-          connection_name="feed",
-          message='#靠北海大二點零{} \n發文網址：http://www.rexwu.taipei \n{}'.format(id, post),
+          message='#靠北海大二點零{} \n發文網址：http://www.rexwu.taipei \n{}'.format(self.id, self.post),
         ))
